7366b62f7db38fbbfe4b046fc116a7864a17c788
= <gonzalesmarkangeloph@gmail.com>
2021-11-12 07:35:38 +0800

    populate games issue

--- app/controllers/PopulateGamesController.php
+++ app/controllers/PopulateGamesController.php
@@ -6,26 +6,25 @@
 	{
 		public function index()
 		{
+			if( Session::get('games_fetched') )
+			{
+				$fetched_games = intval(Session::get('games_fetched'));
+				$fetched_games += 1;
+
+				if( $fetched_games > 1) {
+					Flash::set("<strong>You can only manually fetched games two times</strong> , let the robot do its job we <strong> collect new games every 5 minutes </strong> to avoid complications" , 'danger');
+					return redirect('MatchesController/index');
+				}
+			}else{
+				Session::set('games_fetched' , 1);
+			}
+
 			$leagueMatchService = new LeagueMatchService();
 			$leagueMatchService->populateMatches();
 
 
 			$dotaMatchService = new DotaMatchService();
 			$dotaMatchService->populateMatches();
-
-
-			if( Session::get('games_fetched') )
-			{
-				$fetched_games = intval(Session::get('games_fetched'));
-				$fetched_games += 1;
-
-				if( $fetched_games < 1) {
-					Flash::set("You can only manually fetched games two times , let the robot do its job to avoid complications" , 'danger');
-					return redirect('MatchesController/index');
-				}
-			}else{
-				Session::set('games_fetched' , 1);
-			}
 
 			Flash::set("Games Fetched");
 			return redirect('MatchesController/index');
