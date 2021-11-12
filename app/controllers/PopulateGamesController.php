b28510a1884a1ab5ddad413ba11f75e278f5d3e1
= <gonzalesmarkangeloph@gmail.com>
2021-11-12 10:49:44 +0800

    populate matches

--- app/controllers/PopulateGamesController.php
+++ app/controllers/PopulateGamesController.php
@@ -11,7 +11,7 @@
 				$fetched_games = intval(Session::get('games_fetched'));
 				$fetched_games += 1;
 
-				if( $fetched_games > 1) {
+				if( $fetched_games > 4) {
 					Flash::set("<strong>You can only manually fetched games two times</strong> , let the robot do its job we <strong> collect new games every 5 minutes </strong> to avoid complications" , 'danger');
 					return redirect('MatchesController/index');
 				}
